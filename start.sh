a43d949c157480d6815614b041c7181b20693e2a
SalemMaina <mainasalemofficial@gmail.com>
2026-09-02 13:18:03 +0300

    Auto-generate slots for all doctors on every deploy (rolling 7-day window)

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" || true
+python manage.py generate_slots --days 7
 exec gunicorn ClinicalBookingSystem.wsgi:application --bind 0.0.0.0:$PORT
